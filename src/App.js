3fe0d9da8200810434001f27ac22ab9e930e5bc9
ohana1997 <nguyendainam197@gmail.com>
2021-11-19 19:20:54 +0700

    add new component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import "./App.css"
+import { Route, Switch, useRouteMatch, Link, useParams } from "react-router-dom"
 import HomePage from "./pages/homepage/homepage.component"
+import ShopPage from "./pages/shop/shop.component"
 
 function App() {
-  return <HomePage />
+  // return <HomePage />
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
